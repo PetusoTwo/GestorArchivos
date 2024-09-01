5402e620da8dcf8523292cd391a60eec208f127c
Fabian <96096173+PetusoTwo@users.noreply.github.com>
2024-09-01 11:08:37 -0500

    Delete deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ejecutar migraciones
-php artisan migrate --force
-
-# Crear superusuario si la variable de entorno está presente
-if [ "$CREATE_SUPER_ADMIN" = "true" ]; then
-  php artisan shield:super-admin --name="Administrador" --email="$SUPER_ADMIN_EMAIL" --password="$SUPER_ADMIN_PASSWORD" --force
-fi
