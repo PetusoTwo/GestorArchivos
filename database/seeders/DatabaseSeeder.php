7147088e55d7f8e43f3d8f6055b6c8742325fe43
PetusoTwo <pedrocespedes456@gmail.com>
2024-08-31 19:25:48 -0500

    dasdas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ShieldSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -21,7 +22,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call([
-            UserSeeder::class,
             ShieldSeeder::class,
         ]);
         
