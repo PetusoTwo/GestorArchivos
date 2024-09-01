57d913698b258d6a1483adb1e999da48cd8fb44c
PetusoTwo <pedrocespedes456@gmail.com>
2024-09-01 13:36:14 -0500

    Seeder modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,7 +21,7 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        
+        Artisan::call('shield:super-admin', ['--user' => 1]);
 
 
     }
